a01532607b48cdeff034c498973bff8b6d8656d6
sethuparvathy <sethuparvathy1990@gmail.com>
2019-01-24 16:12:10 +0530

    sharedpreference

--- app/src/main/java/com/example/sethu/pjct/WelcomeActivity.java
+++ app/src/main/java/com/example/sethu/pjct/WelcomeActivity.java
@@ -1,13 +1,45 @@
 package com.example.sethu.pjct;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class WelcomeActivity extends AppCompatActivity {
-
+String us,ps,checkUser;
+    String getus,getps;
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+      SharedPreferences pref=getSharedPreferences("login",MODE_PRIVATE);
+
+
+
+
+
+
+        b=(Button)findViewById(R.id.logoutbtn) ;
+       b.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               SharedPreferences.Editor edits=getSharedPreferences("login",MODE_PRIVATE).edit();
+               edits.clear();
+               edits.commit();
+               Intent q=new Intent(getApplicationContext(),loginactivity.class);
+               startActivity(q);
+           }
+       });
+//        Intent q=getIntent();
+//        us=q.getStringExtra("username");
+//        ps=q.getStringExtra("password");
+
+
     }
-}
+
+    }
+
--- app/src/main/java/com/example/sethu/pjct/loginactivity.java
+++ app/src/main/java/com/example/sethu/pjct/loginactivity.java
@@ -1,6 +1,7 @@
 package com.example.sethu.pjct;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Region;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,12 +13,21 @@ import android.widget.Toast;
 public class loginactivity extends AppCompatActivity {
     EditText s,p;
     Button b,b1;
-    String st,pasd;
+    String st,pasd,checkUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginactivity);
+        SharedPreferences pref=getSharedPreferences("login",MODE_PRIVATE);
+        checkUser=pref.getString("username",null);
+        if (checkUser!=null)
+        {
+            Intent q=new Intent(getApplicationContext(),WelcomeActivity.class);
+            startActivity(q);
+        }
+
+
         s=(EditText)findViewById(R.id.sd);
         p=(EditText)findViewById(R.id.pass);
         b=(Button)findViewById(R.id.login);
@@ -29,7 +39,17 @@ public class loginactivity extends AppCompatActivity {
                 st=s.getText().toString();
                 pasd=p.getText().toString();
                 if (st.equals("mzc")&&pasd.equals("college")){
+                    SharedPreferences.Editor editor=getSharedPreferences("login",MODE_PRIVATE).edit();
+                    editor.putString("username",st);
+                    editor.putString("password",pasd);
+                    editor.apply();
+
                     Intent q=new Intent(getApplicationContext(),WelcomeActivity.class);
+                    q.putExtra("username",st);
+
+
+
+
                     startActivity(q);
                 }
                 else {
